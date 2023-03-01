4565d446ec427d33fdd04ede09f95229eab601b0
nishino <nishino2nd@outlook.jp>
2023-03-01 20:00:49 +0900

    add `createHistory` method

--- src/logic/AbstractSearchLogic.java
+++ src/logic/AbstractSearchLogic.java
@@ -12,10 +12,11 @@ public abstract class AbstractSearchLogic implements SearchLogic {
     if (start != null && goal != null) {
       if (search(graph, start, goal)) {
         var route = createRoute(graph, start, goal);
-        return new SearchResult(graph, route, from, to);
+        var history = createHistory(graph, start, goal);
+        return new SearchResult(graph, from, to, route, history);
       }
     }
-    return new SearchResult(graph, Route.empty(), from, to);
+    return new SearchResult(graph, from, to, Route.empty(), History.empty());
   }
 
   // 子クラスで実装する検索メソッド
@@ -45,5 +46,16 @@ public abstract class AbstractSearchLogic implements SearchLogic {
     
     var route = new Route(links);
     return route;
+  }
+
+  // ステップ数順にノードを並べ替えて履歴を作成
+  private History createHistory(Graph graph, Node start, Node goal) {
+    var orderedNodes = graph.getNodes().stream()
+      .filter(node -> node.getStep() > 0)
+      .sorted(Comparator.comparingInt(Node::getStep))
+      .toList();
+
+    var history = new History(orderedNodes);
+    return history;
   }
 }
