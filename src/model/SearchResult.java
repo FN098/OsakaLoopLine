79212fd2222dbc0c1c108b808522dc349efe97d9
nishino <nishino2nd@outlook.jp>
2023-03-01 19:59:59 +0900

    add `history` property

--- src/model/SearchResult.java
+++ src/model/SearchResult.java
@@ -2,23 +2,26 @@ package model;
 
 public final class SearchResult {
   private final Graph graph;
-  private final Route route;
   private final Object from;
   private final Object to;
+  private final Route route;
+  private final History history;
 
-  public SearchResult(Graph graph, Route route, Object from, Object to) {
+  public SearchResult(
+      Graph graph, 
+      Object from, 
+      Object to, 
+      Route route, 
+      History history) {
     this.graph = graph;
-    this.route = route;
     this.from = from;
     this.to = to;
+    this.route = route;
+    this.history = history;
   }
 
   public Graph getGraph() {
     return graph;
-  }
-
-  public Route getRoute() {
-    return route;
   }
 
   public Object getFrom() {
@@ -27,5 +30,13 @@ public final class SearchResult {
 
   public Object getTo() {
     return to;
+  }
+
+  public Route getRoute() {
+    return route;
+  }
+
+  public History getHistory() {
+    return history;
   }
 }
