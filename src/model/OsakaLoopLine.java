7b375056bf997c628154b14d5267c849e0fd3103
nishino <nishino2nd@outlook.jp>
2023-03-01 20:00:28 +0900

    fix

--- src/model/OsakaLoopLine.java
+++ src/model/OsakaLoopLine.java
@@ -4,14 +4,14 @@ import java.util.*;
 public final class OsakaLoopLine {
 
   public static Graph createGraph() {
-    String graphName = "大阪環状線";
+    var title = "大阪環状線";
 
-    int num = 0;
+    var num = 0;
     var osaka = createNode(num++, "大阪");
     var temma = createNode(num++, "天満");
     var sakuranomiya = createNode(num++, "桜ノ宮");
     var kyobashi = createNode(num++, "京橋");
-    var osakajo = createNode(num++, "大阪城");
+    var osakajokoen = createNode(num++, "大阪城公園");
     var morinomiya = createNode(num++, "森ノ宮");
     var tamatsukuri = createNode(num++, "玉造");
     var tsuruhashi = createNode(num++, "鶴橋");
@@ -35,10 +35,10 @@ public final class OsakaLoopLine {
       new Link(sakuranomiya, temma, 50),
       new Link(sakuranomiya, kyobashi, 50),
       new Link(kyobashi, sakuranomiya, 50),
-      new Link(kyobashi, osakajo, 50),
-      new Link(osakajo, kyobashi, 50),
-      new Link(osakajo, morinomiya, 50),
-      new Link(morinomiya, osakajo, 50),
+      new Link(kyobashi, osakajokoen, 50),
+      new Link(osakajokoen, kyobashi, 50),
+      new Link(osakajokoen, morinomiya, 50),
+      new Link(morinomiya, osakajokoen, 50),
       new Link(morinomiya, tamatsukuri, 55),
       new Link(tamatsukuri, morinomiya, 55),
       new Link(tamatsukuri, tsuruhashi, 55),
@@ -74,7 +74,7 @@ public final class OsakaLoopLine {
       .toList();
 
     var graph = new Graph(nodes, links);
-    graph.setName(graphName);
+    graph.setTitle(title);
     return graph;
   }
 
