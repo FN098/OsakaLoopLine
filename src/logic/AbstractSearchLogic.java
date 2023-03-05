ea07d2973b3dcc5f2b4f506b40bedb16f01c3cf6
nishino <nishino2nd@outlook.jp>
2023-03-05 22:39:09 +0900

    refactoring

--- src/logic/AbstractSearchLogic.java
+++ src/logic/AbstractSearchLogic.java
@@ -1,7 +1,5 @@
 package logic;
 
-import java.util.*;
-
 import model.*;
 
 public abstract class AbstractSearchLogic implements SearchLogic {
@@ -10,22 +8,16 @@ public abstract class AbstractSearchLogic implements SearchLogic {
   public SearchResult search(Graph graph, Object from, Object to) {
     var start = graph.findNodeByValue(from);
     var goal = graph.findNodeByValue(to);
-    if (Objects.isNull(start) || Objects.isNull(goal)) {  // ノードが見つからなければ探索終了
-      return new SearchResult(graph, from, to, Route.empty(), History.empty());
-    }
+    assert start != null && goal != null;
     
     initialize(graph, start, goal);
 
-    Node head = null;
-    var visited = new ArrayList<Node>();
-    while (!Objects.equals(head, goal)) { // ゴールが見つかるまで探索を続ける
-      head = stepNext(); // 次のノードを探索
-      visited.add(head); // 訪問リストに追加
-    }
+    do {
+      stepNext();
+    } while (!isFoundRoute());
 
-    var parents = getParentNodeMap();
-    var route = createRoute(graph, start, goal, parents);
-    var history = new History(visited);
+    var route = createRoute();
+    var history = createHistory();
     return new SearchResult(graph, from, to, route, history);
   }
   
@@ -36,54 +28,31 @@ public abstract class AbstractSearchLogic implements SearchLogic {
    * @param start 探索開始ノード
    * @param goal 探索終了ノード
    */
-  protected abstract void initialize(Graph graph, Node start, Node goal);
+  public abstract void initialize(Graph graph, Node start, Node goal);
 
   /**
    * 次のノードを探索します。
-   * 
-   * @return 訪問したノード
    */
-  protected abstract Node stepNext();
+  public abstract void stepNext();
 
   /**
-   * 子ノードをキーとし、親ノードを値とするマップを取得します。
+   * 経路が見つかったかどうか判定します。
    * 
-   * @return 親ノードのマップ
+   * @return 経路が見つかっていればtrue
    */
-  protected abstract Map<Node, Node> getParentNodeMap();
+  public abstract boolean isFoundRoute();
 
   /**
-   * スタートからゴールまでのルートを作成します。
+   * 経路を作成します。
    * 
-   * @param graph 探索対象のグラフ
-   * @param start 探索開始ノード
-   * @param goal 探索終了ノード
    * @return Routeオブジェクト
    */
-  private Route createRoute(
-      Graph graph, 
-      Node start, 
-      Node goal,
-      Map<Node, Node> parents) {
+  public abstract Route createRoute();
 
-    // ゴールからスタートまでの親を辿る
-    Node head = goal;
-    var links = new ArrayList<Link>();
-    while (!Objects.equals(head, start)) {
-      var parent = parents.get(head);
-      if (Objects.isNull(parent)) {
-        throw new IllegalStateException("親ノードが見つかりません");
-      }
-      var link = graph.findLink(parent, head);
-      if (Objects.isNull(link)) {
-        throw new IllegalStateException("リンクが見つかりません");
-      }
-      links.add(link);
-      head = parent;
-    }
-    
-    // 経路を反転（スタート→ゴール）
-    Collections.reverse(links);
-    return new Route(links);
-  }
+  /**
+   * 探索履歴を作成します。
+   * 
+   * @return Historyオブジェクト
+   */
+  public abstract History createHistory();
 }
--- src/logic/BreadthFirstSearchLogic.java
+++ src/logic/BreadthFirstSearchLogic.java
@@ -7,13 +7,18 @@ import model.*;
 // 幅優先探索
 public final class BreadthFirstSearchLogic extends AbstractSearchLogic {
   private Graph graph;
+  private Node start;
+  private Node goal;
   private final LinkedList<Node> queue = new LinkedList<>(); // BFSに使用するキュー
-  private final Set<Node> visited = new HashSet<>(); // 探索済みノードのセット
+  private final List<Node> visited = new ArrayList<>(); // 探索済みノードのリスト
   private final Map<Node, Node> parents = new HashMap<>();  // 親ノードのマップ
+  private boolean isFinished = false;
 
   @Override
-  protected void initialize(Graph graph, Node start, Node goal) {
+  public void initialize(Graph graph, Node start, Node goal) {
     this.graph = graph;
+    this.start = start;
+    this.goal = goal;
     this.queue.clear();
     this.visited.clear();
     this.parents.clear();
@@ -23,12 +28,18 @@ public final class BreadthFirstSearchLogic extends AbstractSearchLogic {
   }
 
   @Override
-  protected Node stepNext() {
-    if (queue.isEmpty()) {
-      throw new IllegalStateException("初期化されていないか、探索するノードがありません。");
+  public void stepNext() {
+    if (isFinished) {
+      return;
     }
 
     var head = queue.remove(); // キューから先頭のノードを取り出す
+
+    // ゴールが見つかれば探索終了
+    if (head.equals(goal)) {
+      isFinished = true;
+      return;
+    }
 
     // 隣接するノードが訪問済みでない場合は、キューに追加して訪問済みにする
     graph.findNeighborNodes(head).stream()
@@ -38,12 +49,44 @@ public final class BreadthFirstSearchLogic extends AbstractSearchLogic {
         visited.add(neighbor);
         parents.put(neighbor, head);
       });
-
-    return head;
   }
 
   @Override
-  protected Map<Node, Node> getParentNodeMap() {
-    return Collections.unmodifiableMap(parents);
+  public boolean isFoundRoute() {
+    return isFinished;
+  }
+
+  @Override
+  public Route createRoute() {
+    if (!isFinished) {
+      return Route.empty();
+    }
+
+    var links = new ArrayList<Link>();
+
+    // ゴールからスタートまでの親を辿る
+    Node head = goal;
+    while (!Objects.equals(head, start)) {
+      var parent = parents.get(head);
+      assert parent != null;
+
+      var link = graph.findLink(parent, head);
+      assert link != null;
+      
+      links.add(link);
+      head = parent;
+    }
+    
+    // 経路を反転
+    Collections.reverse(links);
+
+    return new Route(links);
+  }
+
+  public History createHistory() {
+    if (!isFinished) {
+      return History.empty();
+    }
+    return new History(visited);
   }
 }
