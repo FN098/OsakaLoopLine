5979be23a03154c6d7615e9c6403db84d453d48f
nishino <nishino2nd@outlook.jp>
2023-03-05 15:18:42 +0900

    remove mutable properties.

--- src/model/Node.java
+++ src/model/Node.java
@@ -2,9 +2,6 @@ package model;
 
 public final class Node {
   private final Object value;
-  private Node parent = null;
-  private boolean isVisited = false;
-  private int step = 0;
 
   public Node(Object value) {
     this.value = value;
@@ -12,30 +9,6 @@ public final class Node {
 
   public Object getValue() {
     return value;
-  }
-
-  public Node getParent() {
-    return parent;
-  }
-
-  public void setParent(Node parent) {
-    this.parent = parent;
-  }
-
-  public boolean getIsVisited() {
-    return isVisited;
-  }
-
-  public void setIsVisited(boolean isVisited) {
-    this.isVisited = isVisited;
-  }
-
-  public int getStep() {
-    return step;
-  }
-
-  public void setStep(int step) {
-    this.step = step;
   }
 
   @Override
