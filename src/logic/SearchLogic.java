b9dd2f2761cce5cbabb33a23116e5afded191555
nishino <nishino2nd@outlook.jp>
2023-03-05 15:20:47 +0900

    add JDOC.

--- src/logic/SearchLogic.java
+++ src/logic/SearchLogic.java
@@ -3,5 +3,13 @@ package logic;
 import model.*;
 
 public interface SearchLogic {
+  /**
+   * 出発地と目的地を指定してグラフを探索します。
+   * 
+   * @param graph 探索対象のグラフ
+   * @param from 出発地
+   * @param to 目的地
+   * @return 探索結果
+   */
   public SearchResult search(Graph graph, Object from, Object to);
 }
